12789efd4514a2df7c5e8434e9a91c75eb8c28bf
Manasi Rajith <manasirajith238@gmail.com>
2026-05-13 16:40:09 +0530

    Time: 2 ms (14.74%), Space: 12.4 MB (54.03%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def generate(self,numRows):
+
+        triangle = []
+
+        for i in range(numRows):
+
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+            triangle.append(row)
+
+        return triangle
+    def getRow(self, rowIndex):
+        triangle = self.generate(rowIndex + 1)
+        return triangle[rowIndex]
+        
