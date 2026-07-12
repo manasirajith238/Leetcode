135b419151b930f566a2a417560040118f5bd4f2
Manasi Rajith <manasirajith238@gmail.com>
2026-07-12 19:26:04 +0530

    Time: 2 ms (11.94%), Space: 8.6 MB (39.07%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+
+        // Overflow case
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        // Determine sign
+        bool negative = (dividend < 0) ^ (divisor < 0);
+
+        long long dvd = abs((long long)dividend);
+        long long dvs = abs((long long)divisor);
+
+        long long ans = 0;
+
+        while (dvd >= dvs) {
+
+            long long temp = dvs;
+            long long multiple = 1;
+
+            // Find the largest shifted divisor
+            while ((temp << 1) <= dvd) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            ans += multiple;
+        }
+
+        return negative ? -ans : ans;
+    }
+};
